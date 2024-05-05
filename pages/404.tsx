5ad7422877d69fed42808c3da0d6115d07e38abf
Michel Bourbeau <michelbourbeau@gmail.com>
2024-05-05 08:32:29 -0600

    Remove 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,4 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
 const Custom404 = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to the homepage after a delay
-    const timer = setTimeout(() => {
-      router.push('/');
-    }, 3000); // Redirect after 3 seconds
-
-    return () => clearTimeout(timer);
-  }, [router]);
-
   return (
     <div>
       <h1>404 - Page Not Found</h1>
