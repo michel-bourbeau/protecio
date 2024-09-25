a1bcadc71356b28c67551af57ff17a9a5b435e54
Michel Bourbeau <michelbourbeau@gmail.com>
2024-09-25 12:28:26 -0600

    Services Map List Translate

--- components/services/ServicesProtecio.tsx
+++ components/services/ServicesProtecio.tsx
@@ -1,9 +1,17 @@
-import { useTranslation } from 'next-i18next';
+import { Trans, useTranslation } from 'next-i18next';
 import Image from 'next/image';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+const services_professionnels_en_infonuagique = [
+  'ConseilStrategique',
+  'FrameworkDeModernisation',
+  'MesureAdoptionInfonuagique',
+  'FinOp',
+  'Loi25',
+];
 
 export default function ServicesProtecio() {
   const { t } = useTranslation();
@@ -80,171 +88,35 @@ export default function ServicesProtecio() {
         <Container>
           <Row>
             <Col md={12} className="text-center mb-4">
-              <h1>Services professionnels en infonuagique</h1>
+              <h1>
+                {t(
+                  'Services.Services_professionnels_en_infonuagique'
+                )}
+              </h1>
             </Col>
           </Row>
-          <Row>
-            <Col md={3} className="text-center mb-4">
-              <Image
-                src="/images/conseils-strategique.jpg"
-                width={500}
-                height={400}
-                alt={t('Services.ConseilStrategique.title')}
-              />
-            </Col>
-            <Col md={9} className="text-left">
-              <h4>{t('Services.ConseilStrategique.title')}</h4>
-              <ul>
-                <li>Participer ou animer les ateliers de travail</li>
-                <li>
-                  Aligner les requis avec les meilleures pratiques TI
-                  du marché
-                </li>
-                <li>
-                  Livrer des résultats de qualités rapidement en
-                  suivant le modèle agile
-                </li>
-                <li>Répondre aux besoins du CoE Cloud</li>
-                <li>
-                  S’arrimer avec les secteurs, les CoEs et
-                  l’architecture d’entreprise
-                </li>
-                <li>Faire évoluer les centres d’excellence</li>
-                <li>Optmiser la gouvernance</li>
-              </ul>
-            </Col>
-          </Row>
-          {/*           
-          <Row>
-            <Col md={3} className="text-center mb-4">
-              <Image
-                src="/images/exploration-cgp.jpg"
-                width={500}
-                height={400}
-                alt="Exploration Google Cloud Platform"
-              />
-            </Col>
-            <Col md={9} className="text-left">
-              <h4>{t('Services.ExplorationGCP.title')}</h4>
-              <ul>
-                <li>Prise de besoins</li>
-                <li>États des lieux</li>
-                <li>Opportunités d’affaires et technologiques</li>
-                <li>Positionnement sur la platerforme</li>
-                <li>Recommanations et feuille de route</li>
-                <li>Projets et opératon</li>
-              </ul>
-            </Col>
-          </Row> 
-          */}
-          <Row>
-            <Col md={3} className="text-center mb-4">
-              <Image
-                src="/images/framework-de-modernisation.jpg"
-                width={500}
-                height={400}
-                alt={t('Services.FrameworkDeModernisation.title')}
-              />
-            </Col>
-            <Col md={9} className="text-left">
-              <h4>{t('Services.FrameworkDeModernisation.title')}</h4>
-              <ul>
-                <li>
-                  Fermeture des centres de traitements (On-Prem vers
-                  AVS) en cours Préparer la sortie de AVS
-                </li>
-                <li>Collecte des besoins</li>
-                <li>Guide pour la modernisation</li>
-                <li>Prioriser le SaaS, PaaS ou le CaaS</li>
-                <li>
-                  Normaliser les services pour soutenir la
-                  modernisation (cartographie)
-                </li>
-                <li>Chemin de migration (arbre de decision)</li>
-                <li>Architecture de références</li>
-              </ul>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={3} className="text-center mb-4">
-              <Image
-                src="/images/mesure-adoption-de-infonuagique.jpg"
-                width={500}
-                height={400}
-                alt={t('Services.MesureAdoptionInfonuagique.title')}
-              />
-            </Col>
-            <Col md={9} className="text-left">
-              <h4>
-                {t('Services.MesureAdoptionInfonuagique.title')}
-              </h4>
-              <ul>
-                <li>Identification des KPIs</li>
-                <li>Revision du processus</li>
-                <li>Collecte de données</li>
-                <li>Rapports</li>
-                <li>Suivi de performance</li>
-                <li>Aligner les rôles et responsabilités</li>
-                <li>
-                  Suivi des actions à prendre selon la performance
-                </li>
-              </ul>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={3} className="text-center mb-4">
-              <Image
-                src="/images/finops.jpg"
-                width={500}
-                height={400}
-                alt={t('Services.FinOp.title')}
-              />
-            </Col>
-            <Col md={9} className="text-left">
-              <h4>{t('Services.FinOp.title')}</h4>
-              <ul>
-                <li>Optimiser et étendre le processus</li>
-                <li>Instaurer la culture FinOps</li>
-                <li>Responsabilisation</li>
-                <li>Couverture IA</li>
-                <li>
-                  Couverture Azure, GCP, AWS et Exploration SaaS
-                </li>
-              </ul>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={3} className="text-center mb-4">
-              <Image
-                src="/images/loi-25-compliant.jpg"
-                width={500}
-                height={500}
-                alt="Loi 25 compliant"
-              />
-            </Col>
-            <Col md={9} className="text-left">
-              <h4>{t('Services.Loi25.title')}</h4>
-              <ul>
-                <li>
-                  Solution de masquage de données
-                  <ul>
-                    <li>
-                      Datamask sur Google Chrome Solution de
-                      classification MS Purview
-                    </li>
-                    <li>
-                      Accélérateur Solution contre la fuite de données
-                      MS Purview
-                    </li>
-                    <li>
-                      Accélérateur Solution de reprise après sinistre
-                      Azure, GCP et AWS
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </Col>
-          </Row>
+          {services_professionnels_en_infonuagique.map((el) => (
+            <Row>
+              <Col md={3} className="text-center mb-4">
+                <Image
+                  src={`/images/${el}.jpg`} //"/images/conseils-strategique.jpg"
+                  width={500}
+                  height={el === 'Loi25' ? 500 : 400}
+                  alt={t(`Services.${el}.title`)}
+                />
+              </Col>
+              <Col md={9} className="text-left">
+                <h4>{t(`Services.${el}.title`)}</h4>
+                <Trans
+                  i18nKey={t(`Services.${el}.liste`)} // optional -> fallbacks to defaults if not provided
+                  components={{
+                    ul: <ul />,
+                    li: <li />,
+                  }}
+                />
+              </Col>
+            </Row>
+          ))}
         </Container>
       </section>
     </>
