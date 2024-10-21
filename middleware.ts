0514a9f9625e7b040e5b5658bcf154df1dfdd280
Michel Bourbeau <michelbourbeau@gmail.com>
2024-10-21 10:15:50 -0600

    desactive middleare

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,3 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-const PUBLIC_FILE = /\.(.*)$/;
-
 export async function middleware(req: NextRequest) {
-  if (
-    req.nextUrl.pathname.startsWith('/_next') ||
-    req.nextUrl.pathname.includes('/api/') ||
-    PUBLIC_FILE.test(req.nextUrl.pathname)
-  ) {
-    return;
-  }
-
-  /* if (req.nextUrl.locale === 'default') {
-    // Correction ici: utiliser .value au lieu de .valueOf
-    const locale = req.cookies.get('NEXT_LOCALE')?.valueOf || 'fr';
-
-    return NextResponse.redirect(
-      new URL(
-        `/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`,
-        req.url
-      )
-    );
-  } */
+  return; // Middleware désactivé
 }
