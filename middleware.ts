526bb3b87533df7b0dff4ecf11720fdf5fde9dfb
Michel Bourbeau <michelbourbeau@gmail.com>
2024-10-21 10:04:03 -0600

    try fix

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server'
- 
-const PUBLIC_FILE = /\.(.*)$/
- 
+import { NextRequest, NextResponse } from 'next/server';
+
+const PUBLIC_FILE = /\.(.*)$/;
+
 export async function middleware(req: NextRequest) {
   if (
     req.nextUrl.pathname.startsWith('/_next') ||
     req.nextUrl.pathname.includes('/api/') ||
     PUBLIC_FILE.test(req.nextUrl.pathname)
   ) {
-    return
+    return;
   }
- 
+
   if (req.nextUrl.locale === 'default') {
-    const locale = req.cookies.get('NEXT_LOCALE')?.valueOf || 'es'
- 
+    // Correction ici: utiliser .value au lieu de .valueOf
+    const locale = req.cookies.get('NEXT_LOCALE')?.valueOf || 'fr';
+
     return NextResponse.redirect(
-      new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
-    )
+      new URL(
+        `/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`,
+        req.url
+      )
+    );
   }
 }
